87c678cff5fec4f52ddb0e0061c6d6470d6b8153
nramanan <neerajaramanan@gmail.com>
2015-03-18 17:43:19 -0700

    changes to make hn work with installer

--- libs/rest.py
+++ libs/rest.py
@@ -1,8 +1,19 @@
 import requests,sys
 from config_helper import get_proxies
 
-def get(url, proxies=None, auth=None):
-    resp = requests.get(url, proxies=get_proxies(), auth=auth)
+def get_pem():
+    pem_resp = requests.get('http://curl.haxx.se/ca/cacert.pem', proxies=get_proxies())
+    if pem_resp.status_code != 200:
+        print "ERROR: Received bad response from api: %d" % pem_resp.status_code
+        print "API Message: "+pem_resp.text
+        sys.exit()
+    f = open('..\pemfile.pem','w')
+    f.write(pem_resp.text.encode('utf-8'))
+    f.close()
+    return '..\pemfile.pem'
+
+def get(url, proxies=None, auth=None,verify = True):
+    resp = requests.get(url, proxies=get_proxies(), auth=auth, verify=verify)
     if resp.status_code != 200:
         print "ERROR: Received bad response from api: %d" % resp.status_code
         print "API Message: "+resp.text
--- restagents/hackernews.py
+++ restagents/hackernews.py
@@ -6,7 +6,7 @@ import rest
 
 def get_top_stories():
     url = "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
-    return rest.get(url, proxies = get_proxies())
+    return rest.get(url, proxies = get_proxies(), verify=rest.get_pem())
 
 def get_stories(n):
     hn500=get_top_stories()
@@ -15,7 +15,7 @@ def get_stories(n):
     baseurl = "https://hacker-news.firebaseio.com/v0/item/[STORY].json?print=pretty"
     for story in hntopn:
         url=baseurl.replace("[STORY]",str(story))
-        astory = rest.get(url, get_proxies())
+        astory = rest.get(url, get_proxies(), verify=rest.get_pem())
         hits[astory['title']]=astory['url']
         #print "%s:\n%s" % (astory['title'],astory['url'])
     return hits
